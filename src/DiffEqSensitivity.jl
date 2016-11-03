6633d6c72cc775ac1dabeecee2cfcaf94d6791b1
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-02 20:26:34 -0700

    working sensitivity

--- src/DiffEqSensitivity.jl
+++ src/DiffEqSensitivity.jl
@@ -1,5 +1,49 @@
 module DiffEqSensitivity
 
-# package code goes here
+using DiffEqBase
+immutable ODELocalSensitvityFunction{uEltype} <: SensitivityFunction
+  f::ParameterizedFunction
+  J::Matrix{uEltype}
+  df::Vector{uEltype}
+  numparams::Int
+  numindvar::Int
+end
 
+function ODELocalSensitvityFunction(f,u0)
+  numparams = length(f.params)
+  numindvar = length(u0)
+  J = Matrix{eltype(u0)}(numindvar,numindvar)
+  df = Vector{eltype(u0)}(numindvar) # number of funcs size
+  ODELocalSensitvityFunction(f,J,df,numparams,numindvar)
+end
+
+function (S::ODELocalSensitvityFunction)(t,u,du)
+  y = @view u[1:S.numindvar] # These are the independent variables
+  S.f(t,y,@view du[1:S.numindvar]) # Make the first part be the ODE
+  S.f(t,y,S.J,:Jac) # Calculate the parameter Jacobian into J
+  for i in eachindex(S.f.params)
+    Sj = @view u[i*S.numindvar+1:(i+1)*S.numindvar]
+    S.f(t,y,getfield(S.f,S.f.params[i]),S.df,S.f.params[i],:Deriv) # Calculate the parameter derivatives into df
+    du[i*S.numindvar+1:(i+1)*S.numindvar] = S.J*Sj + S.df
+  end
+end
+
+type ODELocalSensitivityProblem{uType,uEltype} <: AbstractODEProblem
+  f::ODELocalSensitvityFunction{uEltype}
+  u₀::uType
+  analytic::Function
+  knownanalytic::Bool
+  numvars::Int
+  isinplace::Bool
+end
+
+function ODELocalSensitivityProblem(f::Function,u0)
+  isinplace = numparameters(f)>=3
+  numvars = length(u0)
+  sense = ODELocalSensitvityFunction(f,u0)
+  sense_u0 = [u0;zeros(numvars*sense.numparams)]
+  ODELocalSensitivityProblem{typeof(u0),eltype(u0)}(sense,sense_u0,(t,u,du)->0,false,numvars,isinplace)
+end
+
+export ODELocalSensitvityFunction, ODELocalSensitivityProblem
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,33 @@
-using DiffEqSensitivity
+using DiffEqSensitivity,OrdinaryDiffEq, ParameterizedFunctions, RecursiveArrayTools
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+f = @ode_def_nohes LotkaVolterra begin
+  dx = a*x - b*x*y
+  dy = -c*y + d*x*y
+end a=>1.5 b=>1 c=>3 d=1
+
+prob = ODELocalSensitivityProblem(f,[1.0;1.0])
+sol = solve(prob,[0;30],alg=:DP8)
+x = vecvec_to_mat([sol[i][1:sol.prob.numvars] for i in 1:length(sol)])
+
+prob2 = ODEProblem(f,[1.0;1.0])
+sol2 = solve(prob2,sol.t,alg=:DP8,adaptive=true,abstol=1)
+x2 = vecvec_to_mat(sol2[:])
+
+@test maximum(x2-x) < 1e-12 # Solve the same problem
+
+# Get the sensitivities
+
+da=[sol[i][sol.prob.numvars+1:sol.prob.numvars*2] for i in 1:length(sol)]
+db=[sol[i][sol.prob.numvars*2+1:sol.prob.numvars*3] for i in 1:length(sol)]
+dc=[sol[i][sol.prob.numvars*3+1:sol.prob.numvars*4] for i in 1:length(sol)]
+
+@test (abs(da[end]) .> abs(da[length(sol)÷2])) == [false;true]
+@test (abs(db[end]) .> abs(db[length(sol)÷2])) == [true;true]
+@test (abs(dc[end]) .> abs(dc[length(sol)÷2])) == [false;true]
+#using Plots
+#gr()
+#plot(sol.t,vecvec_to_mat(x))
+#plot(sol.t,vecvec_to_mat(da))
+#plot(sol.t,vecvec_to_mat(db))
+#plot(sol.t,vecvec_to_mat(dc))
