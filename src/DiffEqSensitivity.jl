0e462739224b8f9ed171ee3010e6ec99583bf47c
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-26 15:17:41 -0700

    add mass matrix

--- src/DiffEqSensitivity.jl
+++ src/DiffEqSensitivity.jl
@@ -33,22 +33,24 @@ function (S::ODELocalSensitvityFunction)(t,u,du)
   end
 end
 
-type ODELocalSensitivityProblem{uType,tType,isinplace,F,C} <: AbstractODEProblem{uType,tType,isinplace}
+type ODELocalSensitivityProblem{uType,tType,isinplace,F,C,MM} <: AbstractODEProblem{uType,tType,isinplace}
   f::F
   u0::uType
   tspan::Tuple{tType,tType}
   indvars::Int
   callback::C
+  mass_matrix::MM
 end
 
-function ODELocalSensitivityProblem(f,u0,tspan;callback=CallbackSet())
+function ODELocalSensitivityProblem(f,u0,tspan;callback=CallbackSet(),mass_matrix=I)
   isinplace = numargs(f)>=3
   indvars = length(u0)
   sense = ODELocalSensitvityFunction(f,u0)
   sense_u0 = [u0;zeros(indvars*sense.numparams)]
   ODELocalSensitivityProblem{typeof(u0),typeof(tspan[1]),
-                             isinplace,typeof(sense),typeof(callback)}(
-                             sense,sense_u0,tspan,indvars,callback)
+                             isinplace,typeof(sense),typeof(callback),
+                             typeof(mass_matrix)}(
+                             sense,sense_u0,tspan,indvars,callback,mass_matrix)
 end
 
 export ODELocalSensitvityFunction, ODELocalSensitivityProblem, SensitivityFunction
