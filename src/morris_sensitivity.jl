6188dce58575fad01c17930f569f721c35fd3f15
Unknown <vaibhavyashdixit@gmail.com>
2018-05-23 05:44:45 +0530

    Changes

--- src/morris_sensitivity.jl
+++ src/morris_sensitivity.jl
@@ -1,4 +1,4 @@
-struct Matrix
+struct MatSpread
     mat::Array{Array{Float64}}
     spread::Float64
 end
@@ -10,15 +10,14 @@ function generate_design_matrix(prob::DEProblem,p_range,p_steps;k = 10)
     i = 1
     while i <= k
         flag = 0
-        indices[rand(1:length(p_range))] += (rand() < 0.5 ? -1 : 1)
-        for j in 1:length(indices)
-            if indices[j] > p_steps[j] 
-                flag = 1
-                indices[j] -=1 
-            elseif indices[j] < 1.0
-                flag = 1
-                indices[j] =1
-            end
+        j = rand(1:length(p_range))
+        indices[j] += (rand() < 0.5 ? -1 : 1)
+        if indices[j] > p_steps[j] 
+            flag = 1
+            indices[j] -=1 
+        elseif indices[j] < 1.0
+            flag = 1
+            indices[j] =1
         end
         if flag == 0
             all_idxs[i] = copy(indices)
@@ -34,7 +33,7 @@ function generate_design_matrix(prob::DEProblem,p_range,p_steps;k = 10)
     B
 end
 
-function calculate_spread(matrix::Array{Array{Float64}})
+function calculate_spread(matrix)
     spread = 0.0
     for i in 2:length(matrix)
         spread += sqrt(sum(abs2.(matrix[i] - matrix[i-1])))
@@ -47,7 +46,7 @@ function sample_matrices(prob::DEProblem,p_range,p_steps;k=10,simulations=50,r=1
     for i in 1:simulations
         mat = generate_design_matrix(prob,p_range,p_steps;k=4)
         spread = calculate_spread(mat)
-        push!(matrix_array,Matrix(mat,spread))
+        push!(matrix_array,MatSpread(mat,spread))
     end
     sort!(matrix_array,by = x -> x.spread,rev=true)
     matrices = [i.mat for i in matrix_array[1:r]]
