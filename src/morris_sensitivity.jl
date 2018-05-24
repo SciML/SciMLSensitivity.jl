5fee9db9ce54e862df5e1160741591f6dc1accd2
Unknown <vaibhavyashdixit@gmail.com>
2018-05-25 03:47:19 +0530

    Added MorrisGlobalSensitivity

--- src/morris_sensitivity.jl
+++ src/morris_sensitivity.jl
@@ -3,7 +3,7 @@ struct MatSpread
     spread::Float64
 end
 
-function generate_design_matrix(prob::DEProblem,p_range,p_steps;k = 10)
+function generate_design_matrix(p_range,p_steps;k = 10)
     ps = [linspace(p_range[i][1],p_range[i][2],p_steps[i]) for i in 1:length(p_range)]
     indices = [rand(1:i) for i in p_steps]
     all_idxs = Vector{typeof(indices)}(k)
@@ -36,14 +36,45 @@ function calculate_spread(matrix)
     spread
 end
 
-function sample_matrices(prob::DEProblem,p_range,p_steps;k=10,simulations=50,r=10)
+function sample_matrices(p_range,p_steps;k=10,simulations=50,r=10)
     matrix_array = []
     for i in 1:simulations
-        mat = generate_design_matrix(prob,p_range,p_steps;k=4)
+        mat = generate_design_matrix(p_range,p_steps;k=k)
         spread = calculate_spread(mat)
         push!(matrix_array,MatSpread(mat,spread))
     end
     sort!(matrix_array,by = x -> x.spread,rev=true)
     matrices = [i.mat for i in matrix_array[1:r]]
     matrices
+end
+
+function MorrisGlobalSensitivity(prob::DEProblem,alg,t,p_range,p_steps;kwargs...)
+    design_matrices = sample_matrices(p_range,p_steps;kwargs...)
+    effects = [Float64[] for i in 1:length(prob.p)]
+    for i in design_matrices
+        for j in 1:2:length(i)
+            del = i[j+1] - i[j]
+            change_index = 0
+            for k in 1:length(del)
+                if abs(del[k]) > 0
+                    change_index = k
+                    break
+                end
+            end
+            del = sum(del)
+            prob1 = remake(prob;p=i[j+1])
+            prob2 = remake(prob;p=i[j])
+            y1 = solve(prob1,alg;saveat=t)
+            y2 = solve(prob2,alg;saveat=t)
+            elem_effect = abs(sum(y1-y2)/del)
+            push!(effects[change_index],elem_effect)
+        end
+    end
+    means = Float64[]
+    variances = Float64[]
+    for i in effects
+        push!(means,mean(i))
+        push!(variances,var(i))
+    end
+    means,variances
 end
