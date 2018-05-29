0e6bc4283f15c6b5b059745adcb22748b266b1bd
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-05-29 12:21:05 -0700

    also do ODE test

--- test/morrismethod.jl
+++ test/morrismethod.jl
@@ -1,16 +1,5 @@
 using OrdinaryDiffEq, ParameterizedFunctions, DiffEqSensitivity
 
-# f = @ode_def LotkaVolterra begin
-#     dx = a*x - b*x*y
-#     dy = -c*y + x*y
-# end a b c
-
-# p = [1.5,1.0,3.0]
-# t = collect(linspace(0,10,200))
-# prob = ODEProblem(f,[1.0;1.0],(0.0,10.0),p)
-# m = DiffEqSensitivity.morris_sensitivity(prob,Tsit5(),t,[[1,2],[1,2],[2,4]],[10,10,10],simulations=100)
-# println(m)
-
 A = reshape([1,0,2,3],2,2)
 function f(p)
     A*p
@@ -24,3 +13,15 @@ m = DiffEqSensitivity.morris_sensitivity(f,[[1,5],[1,5]],[10,10],relative_scale=
 @test m.means[1][2] ≈ 0 atol=1e-12
 @test m.variances[1][2] ≈ 0 atol=1e-12
 @test m.means[2][1] < m.means[2][2]
+
+# Test that ODEs work
+
+f = @ode_def LotkaVolterra begin
+   dx = a*x - b*x*y
+   dy = -c*y + x*y
+end a b c
+
+p = [1.5,1.0,3.0]
+t = collect(linspace(0,10,200))
+prob = ODEProblem(f,[1.0;1.0],(0.0,10.0),p)
+m = DiffEqSensitivity.morris_sensitivity(prob,Tsit5(),t,[[1,2],[1,2],[2,4]],[10,10,10],simulations=100)
