af46530cefff654a987a2243da19da2c4875257e
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-12-01 19:56:51 -0800

    make adjoint test have a non-zero cost at t=0

--- test/adjoint.jl
+++ test/adjoint.jl
@@ -23,8 +23,7 @@ println("Calculate discrete adjoint sensitivities")
 t = 0.0:0.5:10.0 # TODO: Add end point handling for callback
 # g(t,u,i) = (1-u)^2/2, L2 away from 1
 function dg(out,u,p,t,i)
-  @show t,i
-  (out.=1.0.-u)
+  (out.=2.0.-u)
 end
 
 easy_res = adjoint_sensitivities(sol,Vern9(),dg,t,abstol=1e-14,
@@ -45,7 +44,7 @@ function G(p)
   tmp_prob = remake(prob,u0=convert.(eltype(p),prob.u0),p=p)
   sol = solve(tmp_prob,Vern9(),abstol=1e-14,reltol=1e-14,saveat=t)
   A = convert(Array,sol)
-  sum(((1 .- A).^2)./2)
+  sum(((2 .- A).^2)./2)
 end
 G([1.5,1.0,3.0])
 res2 = ForwardDiff.gradient(G,[1.5,1.0,3.0])
