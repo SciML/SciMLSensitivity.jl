7be1b7db4b1d1e00ce6a6bb54fba7f44d0dcf7d5
Mike Innes <mike.j.innes@gmail.com>
2019-01-18 10:32:30 +0000

    rm ReverseDiff

--- src/DiffEqSensitivity.jl
+++ src/DiffEqSensitivity.jl
@@ -2,7 +2,7 @@ __precompile__()
 
 module DiffEqSensitivity
 
-using DiffEqBase, Compat, ForwardDiff, ReverseDiff, Flux, DiffEqDiffTools, Statistics
+using DiffEqBase, Compat, ForwardDiff, Flux, DiffEqDiffTools, Statistics
 using DiffEqCallbacks, QuadGK, RecursiveArrayTools, LinearAlgebra
 
 abstract type SensitivityFunction end
