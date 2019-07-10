48b8b37acd71d35bbc01107005f36a032979dd3f
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-10 06:56:54 -0400

    add downstream testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,16 @@
 using DiffEqSensitivity, SafeTestsets
 using Test
 
-@testset "DiffEqSensitivity" begin
+@time begin
+if GROUP == "All" || GROUP == "Core"
+    @safetestset "Local Sensitivity" begin include("local.jl") end
+    @safetestset "Adjoint Sensitivity" begin include("adjoint.jl") end
+    @safetestset "Morris Method" begin include("morris_method.jl") end
+    @safetestset "Sobol Method" begin include("sobol_method.jl") end
+    @safetestset "DGSM Method" begin include("DGSM.jl") end
+end
 
-@safetestset "Local Sensitivity" begin include("local.jl") end
-@safetestset "Adjoint Sensitivity" begin include("adjoint.jl") end
-@safetestset "Morris Method" begin include("morris_method.jl") end
-@safetestset "Sobol Method" begin include("sobol_method.jl") end
-@safetestset "DGSM Method" begin include("DGSM.jl") end
-
+if GROUP == "Downstream"
+    using Pkg
+    Pkg.test("DiffEqFlux")
 end
