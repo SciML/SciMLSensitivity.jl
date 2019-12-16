e91de2cb3dc1440b4e78ad93e08cf6bfbd5f361a
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-12-17 00:59:32 +0530

    Add second order sobol indices

--- src/sobol_sensitivity.jl
+++ src/sobol_sensitivity.jl
@@ -22,6 +22,21 @@ function fuse_designs(A,B)
     Aᵦ
 end
 
+
+# function fuse_designs_second(A,B)
+#     d = size(A,1)
+#     Aᵦ = [copy(A) for i in 1:(d*(d-1))/2]
+#     k = 1
+#     for i in 1:d
+#         for j in i+1:d
+#             Aᵦ[k][i,:] = B[i,:]
+#             Aᵦ[k][j,:] = B[j,:]
+#             k += 1
+#         end
+#     end
+#     Aᵦ
+# end
+
 function gsa(f,method::Sobol,A::AbstractMatrix,B::AbstractMatrix;
              batch=false,Ei_estimator = :Jansen1999)
     Aᵦ = fuse_designs(A,B)
@@ -46,9 +61,13 @@ function gsa(f,method::Sobol,A::AbstractMatrix,B::AbstractMatrix;
         fA = all_y[1:n]
         fB = all_y[(n+1):(2n)]
         fAⁱ= [all_y[(j*n+1):((j+1)*n)] for j in 2:(d+1)]
-
+        if 2 in method.order 
+            fAⁱʲ= all_y[Int(end - (d*(d-1))/2):end]
+        end
         Vᵢ = [sum(fB.*(fAⁱ[i].-fA)) for i in 1:d]./n
-
+        if 2 in method.order 
+            Vᵢⱼ = [sum(abs2, fAⁱ[i] - fAⁱ[j]) for i in 1:d for j in i+1:d]./(2n)
+        end
         if Ei_estimator == :Homma1996
             Eᵢ = [Vary .- sum(fA .* fAⁱ[i])./(n) + Ey.^2 for i in 1:d]
         elseif Ei_estimator == :Sobol2007
@@ -68,6 +87,9 @@ function gsa(f,method::Sobol,A::AbstractMatrix,B::AbstractMatrix;
 
         Vᵢ = reduce(hcat,[sum(fB.*(fAⁱ[i].-fA),dims=2) for i in 1:d]./n)
 
+        if 2 in method.order 
+            Vᵢⱼ = reduce(hcat, [sum(abs2, fAⁱ[i] - fAⁱ[j]) for i in 1:d for j in i+1:d]./(2n))
+        end
         if Ei_estimator == :Homma1996
             Eᵢ = reduce(hcat,[Vary .- sum(fA .* fAⁱ[i],dims=2)./(n) + Ey.^2 for i in 1:d])
         elseif Ei_estimator == :Sobol2007
@@ -77,11 +99,11 @@ function gsa(f,method::Sobol,A::AbstractMatrix,B::AbstractMatrix;
         end
 
     end
-    #Eᵢⱼ = [sum(abs2,fAⁱ[i] - fAⁱ[j]) for i in 1:d, j in 1:d]./(2n)
-    #Vᵢⱼ = Vary .- Eᵢⱼ
-    #Sᵢⱼ= Vᵢⱼ./Vary
+    if 2 in method.order
+        Sᵢⱼ= (Vᵢⱼ)./Vary
+    end
 
     Sᵢ = Vᵢ ./Vary
     Tᵢ = Eᵢ ./Vary
-    SobolResult(Sᵢ,nothing,nothing,nothing,Tᵢ,nothing)
+    SobolResult(Sᵢ,nothing,2 in method.order ? Sᵢⱼ : nothing ,nothing,Tᵢ,nothing)
 end
