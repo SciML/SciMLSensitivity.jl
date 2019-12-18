ab68df22592cacb7fe2181e89e7c611c768bc425
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-12-19 03:43:35 +0530

    Add multioutput test

--- test/morris_method.jl
+++ test/morris_method.jl
@@ -11,6 +11,23 @@ function linear_batch(X)
     @. A*X[1,:]+B*X[2,:]
 end
 
+function ishi_linear(X)
+    A= 7
+    B= 0.1
+    [sin(X[1]) + A*sin(X[2])^2+ B*X[3]^4 *sin(X[1]),A*X[1]+B*X[2]]
+end
+
+function ishi_linear_batch(X)
+    A= 7
+    B= 0.1
+    X1 = @. sin(X[1,:]) + A*sin(X[2,:])^2+ B*X[3,:]^4 *sin(X[1,:])
+    X2 = @. A*X[1,:]+B*X[2,:]
+    vcat(X1',X2')
+end
+
+lb = -ones(4)*π
+ub = ones(4)*π
+
 m = gsa(f_morris,Morris(p_steps=[10,10],total_num_trajectory=1000,num_trajectory=150),[[1,5],[1,5]])
 @test m.means[:,1] ≈ A[:,1] atol=1e-12
 @test m.means[:,2] ≈ A[:,2] atol=1e-12
@@ -23,3 +40,9 @@ m = gsa(f_morris,Morris(p_steps=[10,10],relative_scale=true,total_num_trajectory
 
 m = gsa(linear_batch,Morris(p_steps=[10,10],relative_scale=true,num_trajectory=10000),[[1,5],[1,5]],batch=true)
 @test m.means ≈ [0.439769  0.00579919] atol = 1e-2
+
+m = gsa(ishi_linear,Morris(p_steps=[10,10],relative_scale=false,num_trajectory=100),[[lb[i],ub[i]] for i in 1:4])
+@test m.means[2,:] ≈ [7.0,0.1,0.0,0.0] rtol = 1e-12
+
+m = gsa(ishi_linear_batch,Morris(p_steps=[10,10],relative_scale=false,num_trajectory=100),[[lb[i],ub[i]] for i in 1:4])
+@test m.means[2,:] ≈ [7.0,0.1,0.0,0.0] rtol = 1e-12
