ed98720de9b074038e100f4c66eb36103f4f031d
Yingbo Ma <mayingbo5@gmail.com>
2020-01-07 16:47:06 -0500

    Clean up generate_callbacks

--- src/local_sensitivity/adjoint_common.jl
+++ src/local_sensitivity/adjoint_common.jl
@@ -69,33 +69,30 @@ end
 getprob(S::SensitivityFunction) = S isa ODEBacksolveSensitivityFunction ? S.prob : S.sol.prob
 
 function generate_callbacks(sensefun, g, λ, t, callback, init_cb)
-  if sensefun.discrete
-    @unpack sensealg, y, sol = sensefun
-    prob = sol.prob
-    cur_time = Ref(length(t))
-    function time_choice(integrator)
-      cur_time[] > 0 ? t[cur_time[]] : nothing
-    end
-    affect! = let isq = (sensealg isa QuadratureAdjoint), λ=λ, t=t, y=y, cur_time=cur_time, idx=length(prob.u0)
-      function (integrator)
-        p, u = integrator.p, integrator.u
-        λ  = isq ? λ : @view(λ[1:idx])
-        g(λ,y,p,t[cur_time[]],cur_time[])
-        if isq
-          u .+= λ
-        else
-          u = @view u[1:idx]
-          u .= λ .+ @view integrator.u[1:idx]
-        end
-        u_modified!(integrator,true)
-        cur_time[] -= 1
-      end
-    end
-    cb = IterativeCallback(time_choice,affect!,eltype(prob.tspan);initial_affect=init_cb)
+  sensefun.discrete || return callback
 
-    _cb = CallbackSet(cb,callback)
-  else
-    _cb = callback
+  @unpack sensealg, y = sensefun
+  prob = getprob(sensefun)
+  cur_time = Ref(length(t))
+  time_choice = let cur_time=cur_time, t=t
+    integrator -> cur_time[] > 0 ? t[cur_time[]] : nothing
   end
-  return _cb
+  affect! = let isq = (sensealg isa QuadratureAdjoint), λ=λ, t=t, y=y, cur_time=cur_time, idx=length(prob.u0)
+    function (integrator)
+      p, u = integrator.p, integrator.u
+      λ  = isq ? λ : @view(λ[1:idx])
+      g(λ,y,p,t[cur_time[]],cur_time[])
+      if isq
+        u .+= λ
+      else
+        u = @view u[1:idx]
+        u .= λ .+ @view integrator.u[1:idx]
+      end
+      u_modified!(integrator,true)
+      cur_time[] -= 1
+      return nothing
+    end
+  end
+  cb = IterativeCallback(time_choice,affect!,eltype(prob.tspan);initial_affect=init_cb)
+  return CallbackSet(cb,callback)
 end
