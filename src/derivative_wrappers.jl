0bcf30be662fa2eee347e81badfef723da0af4b4
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-03-01 14:48:21 -0500

    Allow for alternative choices in the vjp calculation
    Allow for alternative choices in the vjp calculation

--- src/derivative_wrappers.jl
+++ src/derivative_wrappers.jl
@@ -75,7 +75,7 @@ function vecjacobian!(dλ, λ, p, t, S::SensitivityFunction;
   prob = getprob(S)
   f = prob.f
   isautojacvec = get_jacvec(sensealg)
-  if !isautojacvec
+  if isautojacvec isa Bool && !isautojacvec
     @unpack J, uf, f_cache, jac_config = S.diffcache
     if DiffEqBase.has_jac(f)
       f.jac(J,y,p,t) # Calculate the Jacobian into J
@@ -96,7 +96,7 @@ function vecjacobian!(dλ, λ, p, t, S::SensitivityFunction;
       mul!(dgrad',λ',pJ)
     end
     dy !== nothing && f(dy, y, p, t)
-  else
+  elseif isautojacvec === :Tracker || (DiffEqBase.isinplace(prob) && isautojacvec)
     if DiffEqBase.isinplace(prob)
       _dy, back = Tracker.forward(y, prob.p) do u, p
         out_ = map(zero, u)
@@ -108,6 +108,50 @@ function vecjacobian!(dλ, λ, p, t, S::SensitivityFunction;
       dgrad !== nothing && (dgrad[:] .= vec(tmp2))
       dy !== nothing && (dy[:] .= vec(Tracker.data(_dy)))
     else
+      _dy, back = Tracker.forward(y, prob.p) do u, p
+        Tracker.collect(f(u, p, t))
+      end
+      tmp1, tmp2 = Tracker.data.(back(λ))
+      dλ[:] .= vec(tmp1)
+      dgrad !== nothing && (dgrad[:] .= vec(tmp2))
+      dy !== nothing && (dy[:] .= vec(Tracker.data(_dy)))
+    end
+  elseif isautojacvec === :ReverseDiff || (DiffEqBase.isinplace(prob) && isautojacvec)
+    if DiffEqBase.isinplace(prob)
+      tape = ReverseDiff.compile(ReverseDiff.GradientTape((u, p)) do (u,p)
+        du1 = similar(p, size(u))
+        f(du1,u,p,t)
+        return vec(du1)
+      end)
+    else
+      tape = ReverseDiff.compile(ReverseDiff.GradientTape((u, p)) do (u,p)
+        vec(f(u,p,t))
+      end)
+    end
+    tu, tp = ReverseDiff.input_hook(tape)
+    output = ReverseDiff.output_hook(tape)
+    ReverseDiff.unseed!(tu) # clear any "leftover" derivatives from previous calls
+    ReverseDiff.unseed!(tp)
+    ReverseDiff.value!(tu, u)
+    ReverseDiff.value!(tp, p)
+    ReverseDiff.forward_pass!(tape)
+    ReverseDiff.increment_deriv!(output, λ)
+    ReverseDiff.reverse_pass!(tape)
+    copyto!(vec(dλ), ReverseDiff.deriv(tu))
+    dgrad !== nothing && copyto!(vec(dgrad), ReverseDiff.deriv(tp))
+    dy !== nothing && copyto!(vec(dy), ReverseDiff.value(output))
+  elseif isautojacvec === :Zygote || (!DiffEqBase.isinplace(prob) && isautojacvec)
+    if DiffEqBase.isinplace(prob)
+      _dy, back = Zygote.pullback(y, prob.p) do u, p
+        out_ = Zygote.Buffer(u)
+        f(out_, u, p, t)
+        vec(copy(out_))
+      end
+      tmp1,tmp2 = back(λ)
+      dλ[:] .= vec(tmp1)
+      dgrad !== nothing && (dgrad[:] .= vec(tmp2))
+      dy !== nothing && (dy[:] .= vec(_dy))
+    else
       _dy, back = Zygote.pullback(y, prob.p) do u, p
         vec(f(u, p, t))
       end
@@ -116,6 +160,8 @@ function vecjacobian!(dλ, λ, p, t, S::SensitivityFunction;
       dgrad !== nothing && (dgrad[:] .= vec(tmp2))
       dy !== nothing && (dy[:] .= vec(_dy))
     end
+  else
+    throw(error("Invalid choice of autojacvec. Either choose a boolean, :Tracker, :Zygote, or :ReverseDiff"))
   end
   return nothing
 end
