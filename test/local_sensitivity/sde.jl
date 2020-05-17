fdd798319a7e836b6ab57c035278422d69958501
Frank Schaefer <frank.schaefer@unibas.ch>
2020-05-17 20:13:19 +0200

    changed time range, removed duplicate functions

--- test/local_sensitivity/sde.jl
+++ test/local_sensitivity/sde.jl
@@ -1,8 +1,7 @@
-using DiffEqDevTools # for convergence test
 using Test, LinearAlgebra
 using OrdinaryDiffEq
 using DiffEqSensitivity, StochasticDiffEq, DiffEqBase
-using ForwardDiff, Calculus, QuadGK, ReverseDiff
+using ForwardDiff, Calculus, ReverseDiff
 using Random
 using Plots
 
@@ -51,14 +50,15 @@ function G(p)
   tmp_prob = remake(prob_oop_ode,u0=eltype(p).(prob_oop_ode.u0),p=p,
                     tspan=eltype(p).(prob_oop_ode.tspan),abstol=abstol, reltol=reltol)
   sol = solve(tmp_prob,Tsit5(),saveat=t,abstol=abstol, reltol=reltol)
+  #@show sol
   res = g(sol,p,nothing)
   @show res
   res
 end
 res_ode_forward = ForwardDiff.gradient(G,p)
-res_ode_reverse = ReverseDiff.gradient(G,p)
+#res_ode_reverse = ReverseDiff.gradient(G,p)
 @test isapprox(res_ode_forward[1], sum(@. u₀^2*exp(2*p[1]*t)*t), rtol = 1e-4)
-@test isapprox(res_ode_reverse[1], sum(@. u₀^2*exp(2*p[1]*t)*t), rtol = 1e-4)
+#@test isapprox(res_ode_reverse[1], sum(@. u₀^2*exp(2*p[1]*t)*t), rtol = 1e-4)
 @test isapprox(res_ode_p'[1], sum(@. u₀^2*exp(2*p[1]*t)*t), rtol = 1e-4)
 
 
@@ -66,9 +66,37 @@ res_ode_reverse = ReverseDiff.gradient(G,p)
 
 Random.seed!(seed)
 prob_oop_sde = SDEProblem(f_oop_linear,σ_oop_linear,u₀,trange,p)
-sol_oop_sde = solve(prob_oop_sde,RKMil(interpretation=:Stratonovich),dt=tend/100,adaptive=false,save_noise=true)
-res_sde_u0, res_sde_p = adjoint_sensitivities(sol_oop_sde,RKMil(interpretation=:Stratonovich),dg!,t
+sol_oop_sde = solve(prob_oop_sde,RKMil(interpretation=:Stratonovich),dt=tend/20,adaptive=false,save_noise=true)
+# res_sde_u0, res_sde_p = adjoint_sensitivities(sol_oop_sde,RKMil(interpretation=:Stratonovich),dg!,t
+# 	,abstol=abstol,reltol=reltol,sensealg=BacksolveAdjoint()) #
+
+adj_prob2, tstops2 = adjoint_sensitivities(sol_oop_sde,RKMil(interpretation=:Stratonovich),dg!,t
+ 	,abstol=abstol,reltol=reltol,sensealg=BacksolveAdjoint())
+solve(adj_prob2, RKMil(interpretation=:Stratonovich); tstops=tstops2)
+
+
+adj_prob1, tstops1 = adjoint_sensitivities(sol_oop_ode,Tsit5(),dg!,t
 	,abstol=abstol,reltol=reltol,sensealg=BacksolveAdjoint())
+solve(adj_prob1, Tsit5())
+
+
+prob_oop_sde
+adj_prob2
+
+
+
+prob_oop_ode
+adj_prob1
+
+
+
+
+
+
+
+
+
+
 
 
 function GSDE(p)
@@ -91,16 +119,25 @@ res_sde_reverse = ReverseDiff.gradient(GSDE,p)
 
 
 
-# plot(sol_oop_sde)
-# plot!(sol_oop_ode)
-# plot!(t,linear_analytic_ode([0.5],[1.01], t))
 
-adjprob1, adjODE = adjoint_sensitivities(sol_oop_ode,Tsit5(),dg!,t
-		,abstol=abstol,reltol=reltol,sensealg=BacksolveAdjoint())
 
-adjprob2, adjSDE = adjoint_sensitivities(
-	sol_oop_sde,RKMil(interpretation=:Stratonovich),dg!,t
-	,abstol=abstol,reltol=reltol,
-	sensealg=BacksolveAdjoint())
-
-#plot(adjSDE)
+# using StochasticDiffEq, DiffEqNoiseProcess, Plots
+# α=1
+# β=1
+# u₀=1/2
+# f(u,p,t) = α*u
+# g(u,p,t) = β*u
+# dt = 1//2^(4)
+# tspan = (0.0,1.0)
+# prob = SDEProblem(f,g,u₀,(0.0,1.0))
+# sol = solve(prob,EulerHeun(),dt=0.01,save_noise=true)
+# _sol = deepcopy(sol) # to make sure the plot is correct
+# W3 = NoiseGrid(reverse!(_sol.t),reverse!(_sol.W))
+#
+# f!(du,u,p,t) = @. du=α*u
+# g!(du,u,p,t) = @. du=β*u
+#
+# prob3 = SDEProblem(f!,g!,[0.0,0.0,0.0,sol[end]],(1.0,0.0),noise=W3)
+# sol2 = solve(prob3,EulerHeun(),dt=0.01)
+# plot(sol.t,sol.u)
+# plot!(sol2.t,sol2.u)
