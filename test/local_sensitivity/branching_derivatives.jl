088628c272429cb781a9b8023e4e289dce0ac1d4
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-06-08 23:04:24 -0400

    change default norm to make ForwardDiffSensitivity test pass

--- test/local_sensitivity/branching_derivatives.jl
+++ test/local_sensitivity/branching_derivatives.jl
@@ -6,13 +6,11 @@ function get_param(breakpoints, values, t)
             return values[i]
         end
     end
-
     return values[end]
 end
 
 function fiip(du, u, p, t)
     a = get_param([1., 2., 3.], p[1:4],  t)
-
     du[1] = dx =  a * u[1] - u[1] * u[2]
     du[2] = dy = -a * u[2] + u[1] * u[2]
 end
@@ -20,13 +18,16 @@ end
 p = [1., 1., 1., 1.]; u0 = [1.0;1.0]
 prob = ODEProblem(fiip, u0, (0.0, 4.0), p);
 
-dp1 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, sensealg = ForwardDiffSensitivity(), saveat = 0.1, abstol=1e-12, reltol=1e-12)), p)
+dp1 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, sensealg = ForwardDiffSensitivity(), internalnorm=internalnorm, saveat = 0.1, abstol=1e-12, reltol=1e-12)), p)
 dp2 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, sensealg = ForwardSensitivity(), saveat = 0.1, abstol=1e-12, reltol=1e-12)), p)
 dp3 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, saveat = 0.1, abstol=1e-12, reltol=1e-12)), p)
 dp4 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, saveat = 0.1, abstol=1e-12, reltol=1e-12, sensealg = InterpolatingAdjoint(autojacvec=ReverseDiffVJP()))), p)
 dp5 = Zygote.gradient(p->sum(solve(prob, Tsit5(), u0=u0, p=p, saveat = 0.1, abstol=1e-12, reltol=1e-12, sensealg = InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))), p)
 
-@test_broken dp1[1] ≈ dp2[1]
+internalnorm(u::ForwardDiff.Dual,::Any) = DiffEqBase.UNITLESS_ABS2(ForwardDiff.value(u)) + sum(DiffEqBase.UNITLESS_ABS2.(ForwardDiff.partials(u)))
+internalnorm(u::AbstractArray{<:ForwardDiff.Dual},::Any) = sqrt(sum(x->(DiffEqBase.UNITLESS_ABS2(ForwardDiff.value(x)) + sum(DiffEqBase.UNITLESS_ABS2.(ForwardDiff.partials(x)))),u) / length(u))
+
+@test dp1[1] ≈ dp2[1]
 @test dp2[1] ≈ dp3[1]
 @test dp2[1] ≈ dp4[1]
 @test sum(dp4[1]) ≈ sum(dp5[1])
