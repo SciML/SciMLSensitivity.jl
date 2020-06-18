c7712735e1102f6e6fbc947425c0c1f5e4f0534f
Yingbo Ma <mayingbo5@gmail.com>
2020-06-18 13:19:01 -0400

    Add the missing dgdp term in the sensibility integral

---
+++ test/local_sensitivity/adjoint_param.jl
@@ -0,0 +1,34 @@
+using Test
+using OrdinaryDiffEq
+using DiffEqSensitivity
+using DiffEqBase
+using ForwardDiff
+using QuadGK
+
+function pendulum_eom(dx, x, p, t)
+    dx[1] = x[2]
+    dx[2] = -sin(x[1]) + (-p[1]*sin(x[1]) + p[2]*x[2])  # Second term is a simple controller that stabilizes π
+end
+
+x0 = [0.1, 0.0]
+tspan = (0.0, 10.0)
+p = [-24.05, -19.137]
+prob = ODEProblem(pendulum_eom, x0, tspan, p)
+sol = solve(prob, Vern9(), abstol=1e-8, reltol=1e-8)
+
+g(x, p, t) = 1.0*(x[1] - π)^2 + 1.0*x[2]^2 + 5.0*(-p[1]*sin(x[1]) + p[2]*x[2])^2
+dgdu(out, y, p, t) = ForwardDiff.gradient!(out, y -> g(y, p, t), y)
+dgdp(out, y, p, t) = ForwardDiff.gradient!(out, p -> g(y, p, t), p)
+
+res = adjoint_sensitivities(sol,Vern9(),g,nothing,(dgdu, dgdp),abstol=1e-8,
+                                reltol=1e-8,iabstol=1e-8,ireltol=1e-8)
+
+function G(p)
+    tmp_prob = remake(prob,p=p,u0=convert.(eltype(p), prob.u0))
+    sol = solve(tmp_prob,Vern9(),abstol=1e-8,reltol=1e-8)
+    res,err = quadgk((t)-> g(sol(t), p, t), 0.0,10.0,atol=1e-8,rtol=1e-8)
+    res
+end
+res2 = ForwardDiff.gradient(G,p)
+
+@test res[2]' ≈ res2 atol=1e-5
