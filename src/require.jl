3c1c6afe87a5e33879bd68b4cfe911ed60bb3525
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-06-21 11:19:02 -0400

    CUDA.jl compatibility

--- src/require.jl
+++ src/require.jl
@@ -14,4 +14,19 @@ function __init__()
         isgpu(::Adjoint{<:Any,TrackedArray{<:Any,<:Any,<:CuArrays.CuArray}}) = true
         isgpu(::Transpose{<:Any,TrackedArray{<:Any,<:Any,<:CuArrays.CuArray}}) = true
     end
+
+    @require CUDA="3a865a2d-5b23-5a0f-bc46-62713ec82fae" begin
+        gpu_or_cpu(x::CUDA.CuArray) = CUDA.CuArray
+        gpu_or_cpu(x::TrackedArray{<:Any,<:Any,<:CUDA.CuArray}) = CUDA.CuArray
+        gpu_or_cpu(x::Transpose{<:Any,<:CUDA.CuArray}) = CUDA.CuArray
+        gpu_or_cpu(x::Adjoint{<:Any,<:CUDA.CuArray}) = CUDA.CuArray
+        gpu_or_cpu(x::Adjoint{<:Any,TrackedArray{<:Any,<:Any,<:CUDA.CuArray}}) = CUDA.CuArray
+        gpu_or_cpu(x::Transpose{<:Any,TrackedArray{<:Any,<:Any,<:CUDA.CuArray}}) = CUDA.CuArray
+        isgpu(::CUDA.CuArray) = true
+        isgpu(::TrackedArray{<:Any,<:Any,<:CUDA.CuArray}) = true
+        isgpu(::Transpose{<:Any,<:CUDA.CuArray}) = true
+        isgpu(::Adjoint{<:Any,<:CUDA.CuArray}) = true
+        isgpu(::Adjoint{<:Any,TrackedArray{<:Any,<:Any,<:CUDA.CuArray}}) = true
+        isgpu(::Transpose{<:Any,TrackedArray{<:Any,<:Any,<:CUDA.CuArray}}) = true
+    end
 end
