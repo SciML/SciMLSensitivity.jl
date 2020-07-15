e67a700b9e35bf04bc6279aa406d4fc6c4c74979
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-07-14 23:21:35 -0400

    fix test

--- test/local_sensitivity/adjoint_shapes.jl
+++ test/local_sensitivity/adjoint_shapes.jl
@@ -6,7 +6,7 @@ p = randn(3, 4)
 f(u,p,t) = u .* p
 f(du,u,p,t) = (du .= u .* p)
 prob_ube = ODEProblem{false}(f, X, tspan, p)
-Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p, saveat=tstop)),p)
+Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p)),p)
 
 prob_ube = ODEProblem{true}(f, X, tspan, p)
-Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p, saveat=tstop)),p)
+Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p)),p)
