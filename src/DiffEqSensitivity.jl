df15dc7c2a40d1a68aa4fd6dba04359bc96e8b6f
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-07-17 21:06:49 -0400

    add DistributionsAD

--- src/DiffEqSensitivity.jl
+++ src/DiffEqSensitivity.jl
@@ -6,6 +6,7 @@ using RecursiveArrayTools, QuasiMonteCarlo, Adapt
 using Parameters: @unpack, @with_kw
 using FFTW, Distributions, Requires
 using StochasticDiffEq
+using DistributionsAD
 import DiffEqNoiseProcess
 import ZygoteRules, Zygote, ReverseDiff
 
