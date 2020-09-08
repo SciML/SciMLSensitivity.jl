ff5fa1a60599913c1c25e9bfb25e8406a3ee5262
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-09-08 00:49:42 -0400

    Add test for adjoint ReverseDiff TrackedArray
    https://github.com/SciML/DiffEqSensitivity.jl/issues/330

--- test/local_sensitivity/adjoint_shapes.jl
+++ test/local_sensitivity/adjoint_shapes.jl
@@ -10,3 +10,28 @@ Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p)),p)
 
 prob_ube = ODEProblem{true}(f, X, tspan, p)
 Zygote.gradient(p->sum(solve(prob_ube, Midpoint(), u0 = X, p = p)),p)
+
+function aug_dynamics!(dz, z, K, t)
+    x = @view z[2:end]
+    u = -K * x
+    dz[1] = x' * x + u' * u
+    dz[2:end] = x + u
+end
+
+policy_params = ones(2, 2)
+z0 = zeros(3)
+fwd_sol = solve(
+    ODEProblem(aug_dynamics!, z0, (0.0, 1.0), policy_params),
+    Tsit5()
+    u0 = z0,
+    p = policy_params,
+)
+
+solve(
+    ODEAdjointProblem(
+        fwd_sol,
+        InterpolatingAdjoint(),
+        (out, x, p, t, i) -> (out .= 1),
+        [1.0],
+    ),Tsit5()
+)
