b1f848f6763f981446ea2b5620de8259deeb5d65
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-09 23:06:07 -0400

    fix shape tests

--- test/local_sensitivity/adjoint_shapes.jl
+++ test/local_sensitivity/adjoint_shapes.jl
@@ -22,7 +22,35 @@ policy_params = ones(2, 2)
 z0 = zeros(3)
 fwd_sol = solve(
     ODEProblem(aug_dynamics!, z0, (0.0, 1.0), policy_params),
-    Tsit5()
+    Tsit5(),
+    u0 = z0,
+    p = policy_params)
+
+solve(
+    ODEAdjointProblem(
+        fwd_sol,
+        InterpolatingAdjoint(),
+        (out, x, p, t, i) -> (out .= 1),
+        [1.0],
+    ),Tsit5()
+)
+
+A = ones(2, 2)
+B = ones(2, 2)
+Q = ones(2, 2)
+R = ones(2, 2)
+
+function aug_dynamics!(dz, z, K, t)
+    x = @view z[2:end]
+    u = -K * x
+    dz[1] = x' * Q * x + u' * R * u
+    dz[2:end] = A * x + B * u # or just `x + u`
+end
+
+policy_params = ones(2, 2)
+z0 = zeros(3)
+fwd_sol = solve(
+    ODEProblem(aug_dynamics!, z0, (0.0, 1.0), policy_params),
     u0 = z0,
     p = policy_params,
 )
@@ -33,5 +61,5 @@ solve(
         InterpolatingAdjoint(),
         (out, x, p, t, i) -> (out .= 1),
         [1.0],
-    ),Tsit5()
+    ),
 )
