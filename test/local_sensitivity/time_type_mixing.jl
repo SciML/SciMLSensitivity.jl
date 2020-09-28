40ab0a9cee9dc02f813481895e654a575607d2d2
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-28 14:00:35 -0400

    fix length call

--- test/local_sensitivity/time_type_mixing.jl
+++ test/local_sensitivity/time_type_mixing.jl
@@ -15,7 +15,7 @@ function predict_neuralode(p)
     Array(solve(_prob,Tsit5(), saveat=0.1))
 end
 
-loss(p) = sum(abs2,predict_neuralode(p))/length(y)
+loss(p) = sum(abs2,predict_neuralode(p))/length(p)
 
 p_model_ini = copy(p_model)
 
