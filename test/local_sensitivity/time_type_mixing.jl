49cddde303ff68c026439a44035afb5d100275f4
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-09-28 11:21:52 -0400

    simplify the test

--- test/local_sensitivity/time_type_mixing.jl
+++ test/local_sensitivity/time_type_mixing.jl
@@ -1,34 +1,11 @@
 using OrdinaryDiffEq, Zygote, DiffEqSensitivity, Test
 
-# excitation of the system
-no_samples = 1000
-
-datasize = 1000
-tspan = (0.0f0, 99.9f0)
-tsteps = range(tspan[1], tspan[2], length = datasize)
-
-ex = t->sin(t)
-u = ex.(tsteps)
-
-f(x) = (atan(8.0 * x - 4.0) + atan(4.0)) / (2.0 * atan(4.0))
-function hammerstein_system(u)
-    y= zeros(size(u))
-    for k in 2:length(u)
-        y[k] = 0.2 * f(u[k-1]) + 0.8 * y[k-1]
-    end
-    return y
-end
-
-y = Float32.(hammerstein_system(ex.(tsteps)))
-
-nn_model = FastChain(FastDense(1,50, sigmoid), FastDense(50,50, sigmoid), FastDense(50, 1))
-
-p_model = initial_params(nn_model)
+p_model = [1f0]
 
 u0 = Float32.([0.0])
 
 function dudt(du, u, p, t)
-    du[1] = nn_model(ex(t),p)[1]
+    du[1] = p[1]
 end
 
 prob = ODEProblem(dudt,u0,tspan)
@@ -38,7 +15,7 @@ function predict_neuralode(p)
     Array(solve(_prob,Tsit5(), saveat=0.1))
 end
 
-loss(p) = sum(abs2,y .- predict_neuralode(p))/length(y)
+loss(p) = sum(abs2,predict_neuralode(p))/length(y)
 
 p_model_ini = copy(p_model)
 
