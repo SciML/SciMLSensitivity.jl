46474bd83691341ffd7fe9d2bb5e0fa782a4fea2
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-12-01 21:00:21 -0500

    section off the DiffEqFlux test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
 const is_TRAVIS = haskey(ENV,"TRAVIS")
 
 @time begin
-#=
+
 if GROUP == "All" || GROUP == "Core1" || GROUP == "Downstream"
     @time @safetestset "Forward Sensitivity" begin include("local_sensitivity/forward.jl") end
     @time @safetestset "Adjoint Sensitivity" begin include("local_sensitivity/adjoint.jl") end
@@ -28,15 +28,13 @@ if GROUP == "All" || GROUP == "Core2"
     @time @safetestset "Steady State Adjoint" begin include("local_sensitivity/steady_state.jl") end
     @time @safetestset "Concrete Solve Derivatives of Second Order ODEs" begin include("local_sensitivity/second_order_odes.jl") end
 end
-=#
+
 if GROUP == "All" || GROUP == "SDE1"
-    #@time @safetestset "SDE Adjoint" begin include("local_sensitivity/sde_stratonovich.jl") end
-    #@time @safetestset "SDE Scalar Noise" begin include("local_sensitivity/sde_scalar_stratonovich.jl") end
-    #@time @safetestset "SDE Checkpointing" begin include("local_sensitivity/sde_checkpointing.jl") end
-    @time @safetestset "SDE - Neural" begin include("local_sensitivity/sde_neural.jl") end
+    @time @safetestset "SDE Adjoint" begin include("local_sensitivity/sde_stratonovich.jl") end
+    @time @safetestset "SDE Scalar Noise" begin include("local_sensitivity/sde_scalar_stratonovich.jl") end
+    @time @safetestset "SDE Checkpointing" begin include("local_sensitivity/sde_checkpointing.jl") end
 end
 
-#=
 if GROUP == "All" || GROUP == "SDE2"
     @time @safetestset "SDE Non-Diagonal Noise" begin include("local_sensitivity/sde_nondiag_stratonovich.jl") end
 end
@@ -46,6 +44,9 @@ if GROUP == "All" || GROUP == "SDE3"
     @time @safetestset "SDE Ito Scalar Noise" begin include("local_sensitivity/sde_scalar_ito.jl") end
 end
 
+if GROUP == "DiffEqFlux"
+    @time @safetestset "SDE - Neural" begin include("downstream/sde_neural.jl") end
+end
 
 if GROUP == "All" || GROUP == "GSA"
     @time @safetestset "Morris Method" begin include("global_sensitivity/morris_method.jl") end
@@ -54,5 +55,5 @@ if GROUP == "All" || GROUP == "GSA"
     @time @safetestset "eFAST Method" begin include("global_sensitivity/eFAST_method.jl") end
     @time @safetestset "RegressionGSA Method" begin include("global_sensitivity/regression_sensitivity.jl") end
 end
-=#
+
 end
