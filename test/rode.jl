652153353b88380dc25bdb1ae4b657eb2b91cb46
Frank Schaefer <frank.schaefer@unibas.ch>
2021-03-26 18:50:57 +0100

    simplify test case

--- test/rode.jl
+++ test/rode.jl
@@ -181,19 +181,6 @@ end
   @test dpReverseDiff ≈ dp' rtol=1e-2
 
   # isautojacvec = false and with jac and paramjac
-  function jac(J,u,p,t,W)
-    J[1,1] = p[1]*sin(W[1] - W[2])
-    J[2,1] = zero(u[1])
-    J[1,2] = zero(u[1])
-    J[2,2] = p[2]*cos(W[1] + W[2])
-  end
-
-  function paramjac(J,u,p,t,W)
-    J[1,1] = u[1]*sin(W[1] - W[2])
-    J[2,1] = zero(u[1])
-    J[1,2] = zero(u[1])
-    J[2,2] = u[2]*cos(W[1] + W[2])
-  end
   Random.seed!(seed)
   faug = RODEFunction(f,jac=jac,paramjac=paramjac)
   prob_aug = RODEProblem{true}(faug,u0,tspan,p)
@@ -371,19 +358,6 @@ end
   @test dpReverseDiff ≈ dp' rtol=1e-2
 
   # isautojacvec = false and with jac and paramjac
-  function jac(J,u,p,t,W)
-    J[1,1] = p[1]*sin(W[1] - W[2])
-    J[2,1] = zero(u[1])
-    J[1,2] = zero(u[1])
-    J[2,2] = p[2]*cos(W[1] + W[2])
-  end
-
-  function paramjac(J,u,p,t,W)
-    J[1,1] = u[1]*sin(W[1] - W[2])
-    J[2,1] = zero(u[1])
-    J[1,2] = zero(u[1])
-    J[2,2] = u[2]*cos(W[1] + W[2])
-  end
   Random.seed!(seed)
   faug = RODEFunction(f,jac=jac,paramjac=paramjac)
   prob_aug = RODEProblem{false}(faug,u0,tspan,p)
@@ -393,22 +367,20 @@ end
 
   @test du0ReverseDiff ≈ du0 rtol=1e-2
   @test dpReverseDiff ≈ dp' rtol=1e-2
-
 end
 
 
 function f(u,p,t,W)
-  dx = p[1]*u[1]*sin(W[1] - W[2])
-  dy = p[2]*u[2]*cos(W[1] + W[2])
-  return [dx,dy]
+  dx = p[1]*u[1]*sin(W[1])
+  return [dx]
 end
 
 
-dt = 1e-4
-u0 = [1.00;1.00]
+dt = 1e-2
+u0 = [1.00]
 tspan = (0.0,1.0)
 t = tspan[1]:0.1:tspan[2]
-p = [2.0,-2.0]
+p = [2.0]
 
 prob = RODEProblem{false}(f,u0,tspan,p)
 Random.seed!(seed)
@@ -418,8 +390,14 @@ _sol = deepcopy(sol)
 noise_reverse = NoiseGrid(reverse(_sol.t),reverse(_sol.W.W))
 prob_reverse = RODEProblem(f,_sol[end],reverse(tspan),p,noise=noise_reverse)
 sol_reverse = solve(prob_reverse,RandomEM(),dt=dt)
-@test sol(t).u ≈ sol_reverse(t).u rtol=1e-3
+@test sol(t).u ≈ sol_reverse(t).u rtol=1e-2
 @show minimum(sol.u)
+
+# check with forward noise
+noise_forward = DiffEqNoiseProcess.NoiseWrapper(_sol.W)
+prob_forward = RODEProblem(f,_sol(0.0),tspan,p,noise=noise_forward)
+sol_forward = solve(prob_forward,RandomEM(),dt=dt)
+@test sol(t).u ≈ sol_forward(t).u atol=1e-12
 
 # Test if Forward and ReverseMode AD agree.
 Random.seed!(seed)
@@ -428,13 +406,14 @@ du0ReverseDiff,dpReverseDiff = Zygote.gradient((u0,p)->sum(
   ,u0,p)
 Random.seed!(seed)
 dForward = ForwardDiff.gradient((θ)->sum(
-  Array(solve(prob,RandomEM(),dt=dt,u0=θ[1:2],p=θ[3:4],saveat=t)).^2/2)
+  #Array(solve(prob,RandomEM(),dt=dt,u0=θ[1:2],p=θ[3:4],saveat=t)).^2/2)
+  Array(solve(prob,RandomEM(),dt=dt,u0=θ[1:1],p=θ[2:2],saveat=t)).^2/2)
   ,[u0;p])
 
 @info dForward
 
-@test du0ReverseDiff ≈ dForward[1:2]
-@test dpReverseDiff ≈ dForward[3:4]
+@test du0ReverseDiff ≈ dForward[1:1]
+@test dpReverseDiff ≈ dForward[2:2]
 
 # test gradients
 Random.seed!(seed)
@@ -457,29 +436,40 @@ adj_sol_checkpointing = solve(adj_prob_checkpointing, RandomEM(), dt=dt)
 @show adj_sol_checkpointing[end]
 
 
-@test du0ReverseDiff ≈ -adj_sol_checkpointing[end][1:2] rtol=1e-2
-@test dpReverseDiff ≈ adj_sol_checkpointing[end][3:4] rtol=1e-2
+@test du0ReverseDiff ≈ -adj_sol_checkpointing[end][1:1] rtol=1e-2
+@test dpReverseDiff ≈ adj_sol_checkpointing[end][2:2] rtol=1e-2
 
 
-@test du0ReverseDiff ≈ -adj_sol_dense[end][1:2] rtol=1e-2
-@test dpReverseDiff ≈ adj_sol_dense[end][3:4] rtol=1e-2
+@test du0ReverseDiff ≈ -adj_sol_dense[end][1:1] rtol=1e-2
+@test dpReverseDiff ≈ adj_sol_dense[end][2:2] rtol=1e-2
 
 minimum(abs.(adj_sol_checkpointing.W.t - adj_sol_dense.W.t))
+
 
 using Plots
 pl1 = plot(adj_sol_dense, label="Dense")
 plot!(pl1,adj_sol_checkpointing, label="Checkpointing", legend=false)
 
 pl2 = plot(adj_sol_dense.t,hcat(adj_sol_dense-adj_sol_checkpointing ...)')
-
+#savefig(pl1, "sense.png")
 maximum(adj_sol_dense - adj_sol_checkpointing)
 
 adj_prob_backsolve = DiffEqSensitivity.RODEAdjointProblem(sol,BacksolveAdjoint(),dg!,t,nothing)
 adj_sol_backsolve = solve(adj_prob_backsolve, RandomEM(), dt=dt)
 
-plot(getindex.(adj_sol_backsolve(t).u, 3) -getindex.(adj_sol_dense(t).u, 3))
-plot!(getindex.(adj_sol_backsolve(t).u, 4) -getindex.(adj_sol_dense(t).u, 4))
+plot(getindex.(adj_sol_backsolve(t).u, 2) -getindex.(adj_sol_dense(t).u, 2))
+#plot!(getindex.(adj_sol_backsolve(t).u, 4) -getindex.(adj_sol_dense(t).u, 4))
+plot(getindex.(adj_sol_backsolve(t).u, 2) -getindex.(adj_sol_checkpointing(t).u, 2))
+#plot!(getindex.(adj_sol_backsolve(t).u, 4) -getindex.(adj_sol_checkpointing(t).u, 4))
 
 
-plot(getindex.(adj_sol_backsolve(t).u, 3) -getindex.(adj_sol_checkpointing(t).u, 3))
-plot!(getindex.(adj_sol_backsolve(t).u, 4) -getindex.(adj_sol_checkpointing(t).u, 4))
+@test adj_sol_dense.t == adj_sol_checkpointing.t
+@test adj_sol_dense.W.t == adj_sol_checkpointing.W.t
+@test adj_sol_dense.W.W == adj_sol_checkpointing.W.W
+
+
+@test adj_sol_dense.u == adj_sol_checkpointing.u
+
+difference = adj_sol_dense.u - adj_sol_checkpointing.u
+
+@show difference[1:10]
