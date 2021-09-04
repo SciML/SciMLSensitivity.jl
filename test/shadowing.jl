9423989e032f9932deb65604128a0d2c55f0d83f
Frank Schaefer <frank.schaefer@unibas.ch>
2021-09-04 11:56:49 +0200

    add a test for parameter-dependent loss function

--- test/shadowing.jl
+++ test/shadowing.jl
@@ -398,5 +398,59 @@ end
     @test resfw ≈ res atol=1e-1
   end
 
+  @testset "Lorenz parameter-dependent loss function" begin
+    function lorenz!(du,u,p,t)
+      du[1] = p[1]*(u[2]-u[1])
+      du[2] = u[1]*(p[2]-u[3]) - u[2]
+      du[3] = u[1]*u[2] - p[3]*u[3]
+      return nothing
+    end
+
+    u0_trans = rand(3)
+    p = [10.0, 28.0, 8/3]
+
+    # parameter passing to NILSAS
+    M = 2
+    nseg = 40
+    nstep = 101
+
+    tspan_transient = (0.0,30.0)
+    prob_transient = ODEProblem(lorenz!,u0_trans,tspan_transient,p)
+    sol_transient = solve(prob_transient, Tsit5())
+    
+    u0 = sol_transient.u[end]
+
+    tspan_attractor = (0.0,20.0)
+    prob_attractor = ODEProblem(lorenz!,u0,tspan_attractor,p)
+    sol_attractor = solve(prob_attractor,Vern9(),abstol=1e-14,reltol=1e-14,saveat=0.01)
+
+    g(u,p,t) = u[end]^2/2 + sum(p)
+    function dgu(out,u,p,t,i)
+      fill!(out, zero(eltype(u)))
+      out[end] = -u[end]
+    end
+    function dgp(out,u,p,t,i)
+      fill!(out, -one(eltype(p)))
+    end
+
+    lss_problem = ForwardLSSProblem(sol_attractor, ForwardLSS(alpha=10), g, (dgu,dgp))
+    resfw = shadow_forward(lss_problem)
+
+    @info resfw
+
+    nilsas_prob = NILSASProblem(sol_attractor, NILSAS(nseg,nstep,M), g)
+    res = shadow_adjoint(nilsas_prob, Tsit5())
+
+    @info res
+
+    @test resfw ≈ res atol=1e-1
+
+    nilsas_prob = NILSASProblem(sol_attractor, NILSAS(nseg,nstep,M), g, (dgu,dgp))
+    res = shadow_adjoint(nilsas_prob, Tsit5())
+
+    @info res
+
+    @test resfw ≈ res atol=1e-1
+  end
 end
 
