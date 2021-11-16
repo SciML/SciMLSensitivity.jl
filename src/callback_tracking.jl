ee2c743f3825a3d3ec9c13021734a14ae46512ef
Frank Schaefer <frank.schaefer@unibas.ch>
2021-11-16 18:11:50 +0100

    add tprev to FakeIntegrator

--- src/callback_tracking.jl
+++ src/callback_tracking.jl
@@ -54,6 +54,7 @@ end
 
 struct TrackedAffect{T,T2,T3,T4,T5}
     event_times::Vector{T}
+    tprev::Vector{T}
     uleft::Vector{T2}
     pleft::Vector{T3}
     affect!::T4
@@ -61,7 +62,7 @@ struct TrackedAffect{T,T2,T3,T4,T5}
 end
 
 TrackedAffect(t::Number,u,p,affect!::Nothing,correction) = nothing
-TrackedAffect(t::Number,u,p,affect!,correction) = TrackedAffect(Vector{typeof(t)}(undef,0),Vector{typeof(u)}(undef,0),Vector{typeof(p)}(undef,0),affect!,correction)
+TrackedAffect(t::Number,u,p,affect!,correction) = TrackedAffect(Vector{typeof(t)}(undef,0),Vector{typeof(t)}(undef,0),Vector{typeof(u)}(undef,0),Vector{typeof(p)}(undef,0),affect!,correction)
 
 function (f::TrackedAffect)(integrator)
     uleft = deepcopy(integrator.u)
@@ -70,11 +71,13 @@ function (f::TrackedAffect)(integrator)
     if integrator.u_modified
         if isempty(f.event_times)
           push!(f.event_times,integrator.t)
+          push!(f.tprev,integrator.tprev)
           push!(f.uleft,uleft)
           push!(f.pleft,pleft)
         else
           if !maximum(.≈(integrator.t, f.event_times, rtol=0.0, atol=1e-14))
             push!(f.event_times,integrator.t)
+            push!(f.tprev,integrator.tprev)
             push!(f.uleft,uleft)
             push!(f.pleft,pleft)
           end
@@ -121,7 +124,9 @@ struct FakeIntegrator{uType,P,tType}
     u::uType
     p::P
     t::tType
+    tprev::tType
 end
+FakeIntegrator(u,p,t,tprev=t) = FakeIntegrator(u,p,t,tprev)
 
 struct CallbackSensitivityFunction{fType,Alg<:DiffEqBase.AbstractSensitivityAlgorithm,C<:AdjointDiffCache,pType} <: SensitivityFunction
     f::fType
@@ -165,8 +170,11 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
 
     function affect!(integrator)
 
-        function w(du,u,p,t)
-          fakeinteg = FakeIntegrator([x for x in u],[x for x in p],t)
+        indx, pos_neg = get_indx(cb, integrator.t)
+        tprev = get_tprev(cb,indx,pos_neg)
+
+        function w(du,u,p,t,tprev)
+          fakeinteg = FakeIntegrator([x for x in u],[x for x in p],t,tprev)
           _affect! = get_affect!(cb.affect!.affect!)
           _affect!(fakeinteg)
           du .= fakeinteg.u
@@ -175,7 +183,8 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
         S = integrator.f.f # get the sensitivity function
 
         # Create a fake sensitivity function to do the vjps
-        fakeS = CallbackSensitivityFunction(w,sensealg,S.diffcache,integrator.sol.prob)
+        fakeS = CallbackSensitivityFunction((du,u,p,t)->w(du,u,p,t,tprev),sensealg,
+                        S.diffcache,integrator.sol.prob)
 
         du = first(get_tmp_cache(integrator))
         λ,grad,y,dλ,dgrad,dy = split_states(du,integrator.u,integrator.t,S)
@@ -183,7 +192,7 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
         # the QuadratureAdjoint we would need to lift y from the left to the right limit.
         # However, one also needs to update dgrad later on.
         if (sensealg isa QuadratureAdjoint && !cb.save_positions[2]) || (sensealg isa InterpolatingAdjoint && ischeckpointing(sensealg))
-          w(y,y,integrator.p,integrator.t)
+          w(y,y,integrator.p,integrator.t,tprev)
         end 
 
         if cb isa Union{ContinuousCallback,VectorContinuousCallback}
@@ -197,20 +206,20 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
           compute_f!(dy_right,S,y,integrator)
           # if callback did not terminate the time evolution, we have to compute one more correction term.
           if cb.save_positions[2] && !correction.terminated
-            indx = correction.cur_time[] + 1
-            loss_correction!(Lu_right,y,integrator,g,indx)
+            loss_indx = correction.cur_time[] + 1
+            loss_correction!(Lu_right,y,integrator,g,loss_indx)
           else
             Lu_right .*= false
           end
         end
 
-        update_p = copy_to_integrator!(cb,y,integrator.p,integrator.t)
+        update_p = copy_to_integrator!(cb,y,integrator.p,integrator.t,indx,pos_neg)
 
         if cb isa Union{ContinuousCallback,VectorContinuousCallback}
           # compute the correction of the right limit (with left state limit inserted into dgdt)
           @unpack dy_left, cur_time = correction
           compute_f!(dy_left,S,y,integrator)
-          dgdt(dy_left,correction,sensealg,y,integrator)
+          dgdt(dy_left,correction,sensealg,y,integrator,tprev)
           if !correction.terminated
             implicit_correction!(Lu_right,dλ,λ,dy_right,correction)
             correction.terminated = false # additional callbacks might have happened which didn't terminate the time evolution  
@@ -220,14 +229,15 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
         if update_p
           # changes in parameters
           if !(sensealg isa QuadratureAdjoint)
-            function wp(dp,p,u,t)
-              fakeinteg = FakeIntegrator([x for x in u],[x for x in p],t)
+            
+            function wp(dp,p,u,t,tprev)
+              fakeinteg = FakeIntegrator([x for x in u],[x for x in p],t,tprev)
               _affect! = get_affect!(cb.affect!.affect!)
               _affect!(fakeinteg)
               dp .= fakeinteg.p
             end
 
-            fakeSp = CallbackSensitivityFunction(wp,sensealg,S.diffcache,integrator.sol.prob)
+            fakeSp = CallbackSensitivityFunction((du,u,p,t)->wp(du,u,p,t,tprev),sensealg,S.diffcache,integrator.sol.prob)
             #vjp with Jacobin given by dw/dp before event and vector given by grad
             vecjacobian!(dgrad, integrator.p, grad, y, integrator.t, fakeSp;
                                   dgrad=nothing, dy=nothing)
@@ -248,8 +258,8 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
 
           if cb.save_positions[1] == true
             # if the callback saved the first position, we need to implicitly correct this value as well
-            indx = correction.cur_time[] 
-            implicit_correction!(Lu_left,dy_left,correction,y,integrator,g,indx)
+            loss_indx = correction.cur_time[] 
+            implicit_correction!(Lu_left,dy_left,correction,y,integrator,g,loss_indx)
             dλ .+= Lu_left
           end
         end
@@ -273,30 +283,45 @@ function _setup_reverse_callbacks(cb::Union{ContinuousCallback,DiscreteCallback,
                        save_positions = (false,false))
 end
 
+get_indx(cb::DiscreteCallback,t) = (searchsortedfirst(cb.affect!.event_times,t), nothing)
+function get_indx(cb::Union{ContinuousCallback,VectorContinuousCallback}, t)
+  if !isempty(cb.affect!.event_times)
+    indx = searchsortedfirst(cb.affect!.event_times,t)
+    if cb.affect!.event_times[indx]!=t
+      bool = false
+      indx = searchsortedfirst(cb.affect_neg!.event_times,t)
+    else
+      bool = true
+    end
+  else
+    bool = false
+    indx = searchsortedfirst(cb.affect_neg!.event_times,t)
+  end
+  return indx, bool
+end
 
-function copy_to_integrator!(cb::DiscreteCallback, y, p, t)
-  indx = searchsortedfirst(cb.affect!.event_times,t)
+get_tprev(cb::DiscreteCallback,indx,bool) = cb.affect!.tprev[indx]
+function copy_to_integrator!(cb::Union{ContinuousCallback,VectorContinuousCallback}, indx, bool)
+  if bool
+    return cb.affect!.tprev[indx]
+  else
+    return cb.affect_neg!.tprev[indx]
+  end
+end
+
+function copy_to_integrator!(cb::DiscreteCallback, y, p, t, indx, bool)
   copyto!(y, cb.affect!.uleft[indx])
   update_p = (p != cb.affect!.pleft[indx])
   update_p && copyto!(p, cb.affect!.pleft[indx])
   update_p
 end
 
-function copy_to_integrator!(cb::Union{ContinuousCallback,VectorContinuousCallback}, y, p, t)
-  if !isempty(cb.affect!.event_times)
-    indx = searchsortedfirst(cb.affect!.event_times,t)
-    if cb.affect!.event_times[indx]!=t
-      indx = searchsortedfirst(cb.affect_neg!.event_times,t)
-      copyto!(y, cb.affect_neg!.uleft[indx])
-      update_p = (p != cb.affect!.pleft[indx])
-      update_p && copyto!(p, cb.affect_neg!.pleft[indx])
-    else
-      copyto!(y, cb.affect!.uleft[indx])
-      update_p = (p != cb.affect!.pleft[indx])
-      update_p && copyto!(p, cb.affect!.pleft[indx])
-    end
+function copy_to_integrator!(cb::Union{ContinuousCallback,VectorContinuousCallback}, y, p, t, indx, bool)
+  if bool
+    copyto!(y, cb.affect!.uleft[indx])
+    update_p = (p != cb.affect!.pleft[indx])
+    update_p && copyto!(p, cb.affect!.pleft[indx])
   else
-    indx = searchsortedfirst(cb.affect_neg!.event_times,t)
     copyto!(y, cb.affect_neg!.uleft[indx])
     update_p = (p != cb.affect_neg!.pleft[indx])
     update_p && copyto!(p, cb.affect_neg!.pleft[indx])
@@ -315,13 +340,13 @@ function compute_f!(dy,S,y,integrator)
   return nothing
 end
 
-function dgdt(dy,correction,sensealg,y,integrator)
+function dgdt(dy,correction,sensealg,y,integrator,tprev)
   # dy refers to f evaluated on left limit
   @unpack gt_val, gu_val, gt, gu, gt_conf, gu_conf, condition = correction
 
   p, t = integrator.p, integrator.t
 
-  fakeinteg = FakeIntegrator([x for x in y],p,t)
+  fakeinteg = FakeIntegrator([x for x in y],p,t,tprev)
 
   # derivative and gradient of condition with respect to time and state, respectively
   gt.u = y
