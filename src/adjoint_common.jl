063ff078b06e6b7121942c6cb7143d74622b64a4
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-24 11:01:12 -0400

    more blind unwrapping

--- src/adjoint_common.jl
+++ src/adjoint_common.jl
@@ -38,6 +38,10 @@ function adjointdiffcache(g::G, sensealg, discrete, sol, dgdu::DG1, dgdp::DG2, f
     else
         @unpack u0, p, tspan = prob
     end
+
+    # Remove any function wrappers: it breaks autodiff
+    f = unwrapped_f(f)
+
     numparams = p === nothing || p === DiffEqBase.NullParameters() ? 0 : length(p)
     numindvar = length(u0)
     isautojacvec = get_jacvec(sensealg)
