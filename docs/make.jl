d8bc99c92867c83b709074be8b8f556dbc34e71e
Lazaro Alonso <lazarus.alon@gmail.com>
2024-05-31 14:55:20 +0200

    test CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter, SciMLSensitivity
+using DocumenterVitepress
 
 cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
 cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
@@ -13,10 +14,14 @@ makedocs(sitename = "SciMLSensitivity.jl",
     authors = "Chris Rackauckas et al.",
     modules = [SciMLSensitivity],
     clean = true, doctest = false, linkcheck = true,
-    warnonly = [:missing_docs],
-    format = Documenter.HTML(assets = ["assets/favicon.ico"],
-        canonical = "https://docs.sciml.ai/SciMLSensitivity/stable/"),
+    warnonly = true,
+    format = DocumenterVitepress.MarkdownVitepress(
+            repo="https://github.com/SciML/SciMLSensitivity.jl",
+        ),
     pages = pages)
 
 deploydocs(repo = "github.com/SciML/SciMLSensitivity.jl.git";
+    target="build", # this is where Vitepress stores its output
+    branch = "gh-pages",
+    devbranch="master",
     push_preview = true)
