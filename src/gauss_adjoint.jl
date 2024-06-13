999e77007f6f7c9d3fd4fc71076b02d02d212645
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-13 07:12:54 -0400

    Hotfix GaussAdjoint enzyme on non-vector

--- src/gauss_adjoint.jl
+++ src/gauss_adjoint.jl
@@ -474,7 +474,8 @@ function vec_pjac!(out, λ, y, t, S::GaussIntegrand)
         end
     elseif sensealg.autojacvec isa EnzymeVJP
         tmp3, tmp4, tmp6 = paramjac_config
-        tmp4 .= λ
+        vtmp4 = vec(tmp4)
+        vtmp4 .= λ
         out .= 0
         Enzyme.autodiff(
             Enzyme.Reverse, Enzyme.Duplicated(pf, tmp6), Enzyme.Const,
