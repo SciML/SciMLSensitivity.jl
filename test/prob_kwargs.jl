59a965d18ef9cd148414957e5b16238276922eb7
jClugstor <jadonclugston@gmail.com>
2024-07-30 14:06:48 -0400

    add test for ForwardSensitivity callbacks

--- test/prob_kwargs.jl
+++ test/prob_kwargs.jl
@@ -29,3 +29,29 @@ a = ones(3)
 @test Zygote.gradient(f, a)[1][1] ≈ Zygote.gradient(f2, a)[1][1]
 @test Zygote.gradient(f, a)[1][2] == Zygote.gradient(f2, a)[1][2] == 0
 @test Zygote.gradient(f, a)[1][3] == Zygote.gradient(f2, a)[1][3] == 0
+
+# callback in problem construction or in solve call should give same result
+odef(du, u, p, t) = du .= u .* p
+prob = ODEProblem(odef, [2.0], (0.0, 1.0), [3.0])
+
+function f1(u0p)
+    condition(u, t, integrator) = t == 0.5
+    affect!(integrator) = integrator.p[1] += 0.1
+    cb = DiscreteCallback(condition, affect!)
+    prob = ODEProblem{true}(odef, u0p[1:1], (0.0, 1.0), u0p[2:2]; callback = cb)
+    sum(solve(prob, Tsit5(), tstops = [0.5], sensealg = ForwardDiffSensitivity()))
+end
+
+function f2(u0p)
+    condition(u, t, integrator) = t == 0.5
+    affect!(integrator) = integrator.p[1] += 0.1
+    cb = DiscreteCallback(condition, affect!)
+    prob = ODEProblem{true}(odef, u0p[1:1], (0.0, 1.0), u0p[2:2])
+    sum(solve(
+        prob, Tsit5(), tstops = [0.5], callback = cb, sensealg = ForwardDiffSensitivity()))
+end
+u0p = [2.0, 3.0]
+
+@test f1(u0p) == f2(u0p)
+@test Zygote.gradient(f1, u0p)[1] == Zygote.gradient(f2, u0p)[1]
+
