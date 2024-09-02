f5365a2429c282dae76c387d80e6d86c3ed537ca
DhairyaLGandhi <dhairya@juliacomputing.com>
2024-09-02 21:28:22 +0530

    chore: remove check for concrete solve

--- src/concrete_solve.jl
+++ src/concrete_solve.jl
@@ -372,11 +372,11 @@ function DiffEqBase._concrete_solve_adjoint(
         saveat = eltype(prob.tspan)[],
         save_idxs = nothing,
         kwargs...)
-    if !isscimlstructure(p) || !(sensealg isa GaussAdjoint) &&
-       !(p isa Union{Nothing, SciMLBase.NullParameters, AbstractArray}) ||
-       (p isa AbstractArray && !Base.isconcretetype(eltype(p)))
-        throw(AdjointSensitivityParameterCompatibilityError())
-    end
+    # if !(sensealg isa GaussAdjoint) &&
+    #    !(p isa Union{Nothing, SciMLBase.NullParameters, AbstractArray}) ||
+    #    (p isa AbstractArray && !Base.isconcretetype(eltype(p)))
+    #     throw(AdjointSensitivityParameterCompatibilityError())
+    # end
 
     if p === nothing || p isa SciMLBase.NullParameters
         tunables, repack = p, identity
