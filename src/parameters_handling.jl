d2fd79ad4cbc4f61a8dba3a42c9efd47f851654e
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-04-03 13:07:33 +0530

    chore: force allocated buffers for vjp to be deterministic

--- src/parameters_handling.jl
+++ src/parameters_handling.jl
@@ -61,14 +61,14 @@ recursive_add!(::Nothing, ::Nothing) = nothing
 
 `similar(λ, size(x))` for generic `x`. This is used to handle non-array parameters!
 """
-allocate_vjp(λ::AbstractArray, x::AbstractArray) = similar(λ, size(x))
+allocate_vjp(λ::AbstractArray{T}, x::AbstractArray) where T = fill!(similar(λ, size(x)), zero(T))
 allocate_vjp(λ::AbstractArray, x::Tuple) = allocate_vjp.((λ,), x)
 function allocate_vjp(λ::AbstractArray, x::NamedTuple{F}) where {F}
     NamedTuple{F}(allocate_vjp.((λ,), values(x)))
 end
 allocate_vjp(λ::AbstractArray, x) = fmap(Base.Fix1(allocate_vjp, λ), x)
 
-allocate_vjp(x::AbstractArray) = similar(x)
+allocate_vjp(x::AbstractArray) = zero(x) # similar(x)
 allocate_vjp(x::Tuple) = allocate_vjp.(x)
 allocate_vjp(x::NamedTuple{F}) where {F} = NamedTuple{F}(allocate_vjp.(values(x)))
 allocate_vjp(x) = fmap(allocate_vjp, x)
