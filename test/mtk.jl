aaddc02090dd5e9387ec2f9de83959e22a9d74c6
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-04-21 16:54:07 +0530

    test: add test to force DAE initialization and prevent simplification to ODE form; check grads

--- test/mtk.jl
+++ test/mtk.jl
@@ -33,12 +33,52 @@ prob = ODEProblem(sys, u0, tspan, p, jac = true)
 sol = solve(prob, Tsit5())
 mtkparams = SciMLSensitivity.parameter_values(sol)
 
-gt = rand(5501)
+gt = rand(1495)
 dmtk, = Zygote.gradient(mtkparams) do p
-    new_sol = solve(prob, Rosenbrock23(), p = p)
+    new_sol = solve(prob, Tsit5(), p = p)
+    Zygote.ChainRules.ChainRulesCore.ignore_derivatives() do
+        @test all(isapprox.(new_sol[x + y + z + 2 * β - w], 0, atol = 1e-12))
+    end
+    mean(abs.(new_sol[sys.x] .- gt))
+end
+
+# Force DAE handling for Initialization
+
+eqs = [D(D(x)) ~ σ * (y - x),
+    D(y) ~ x * (ρ - z) - y,
+    D(z) ~ x * y - β * z,
+    w ~ x + y + z + 2 * β,
+    0 ~ x^2 + y^2 - w2^2
+]
+
+@mtkbuild sys = ODESystem(eqs, t)
+
+u0 = [D(x) => 2.0,
+    x => 1.0,
+    y => 0.0,
+    z => 0.0,
+    w2 => 0.0,]
+
+p = [σ => 28.0,
+    ρ => 10.0,
+    β => 8 / 3]
+
+tspan = (0.0, 100.0)
+prob = ODEProblem(sys, u0, tspan, p, jac = true)
+sol = solve(prob, Rodas5P(), initializealg = BrownFullBasicInit())
+# sol = solve(prob, Rodas5P())
+mtkparams = SciMLSensitivity.parameter_values(sol)
+
+gt = rand(3609)
+
+sensealg = GaussAdjoint(; autojacvec = SciMLSensitivity.ZygoteVJP())
+dmtk, = Zygote.gradient(mtkparams) do p
+    new_sol = solve(prob, Rodas5P(); p = p, initializealg = BrownFullBasicInit(), sensealg)
     Zygote.ChainRules.ChainRulesCore.ignore_derivatives() do
         @show extrema(new_sol[x + y + z + 2 * β - w])
-        @test all(new_sol[x + y + z + 2 * β - w]  .≈ 0)
+        @show extrema(new_sol[x^2 + y^2 - w2^2])
+        @show mean(new_sol[x^2 + y^2 - w2^2])
+        @test all(isapprox.(new_sol[x + y + z + 2 * β - w], 0, atol = 1e-12))
     end
     mean(abs.(new_sol[sys.x] .- gt))
 end
