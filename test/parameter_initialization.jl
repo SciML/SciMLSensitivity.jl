ee804b291292db854362917e84f11ffcc8e070dc
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-04-23 16:10:32 +0530

    chore: run parameter initialization by passing missing parameters

--- test/parameter_initialization.jl
+++ test/parameter_initialization.jl
@@ -18,7 +18,7 @@ eqs = [D(D(x)) ~ σ * (y - x),
     D(z) ~ x * y - β * z,
     w ~ x + y + z + 2 * β]
 
-@mtkbuild sys = ODESystem(eqs, t)
+@mtkbuild sys = ODESystem(eqs, t, defaults = [ρ => missing,], guesses = [ρ => 10.0])
 
 u0 = [D(x) => 2.0,
     x => 1.0,
@@ -34,6 +34,9 @@ prob = ODEProblem(sys, u0, tspan, p, jac = true)
 sol = solve(prob, Tsit5())
 
 tunables, repack, _ = SS.canonicalize(SS.Tunable(), parameter_values(prob))
+
+
+# f = remake(prob.f; f, initialization_data)
 
 @testset "Adjoint through Parameter Initialization" begin
     @testset "Forward Mode" begin
@@ -89,9 +92,9 @@ tunables, repack, _ = SS.canonicalize(SS.Tunable(), parameter_values(prob))
 
         gs_prob, = Zygote.gradient(tunables) do tunables
             new_prob = remake(prob; p = repack(tunables))
-            sol = solve(new_prob; sensealg)
+            sol = solve(new_prob; sensealg, initializealg = SciMLBase.OverrideInit(), abstol = 1e-6)
             o = sol[w]
-            o[end]
+            o[2]
         end
         @test any(!iszero, gs_prob)
     end
