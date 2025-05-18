239cbc0a6b31dabb4178576043a24627cc6b6e69
Chris Rackauckas <accounts@chrisrackauckas.com>
2025-05-18 23:52:47 +0000

    handle differently

--- src/concrete_solve.jl
+++ src/concrete_solve.jl
@@ -1681,8 +1681,8 @@ function DiffEqBase._concrete_solve_adjoint(prob::SciMLBase.AbstractODEProblem, 
 
     function adjoint_sensitivity_backpass(Δ)
         function df(_out, u, p, t, i)
-            if Δ isa AbstractArray{<:AbstractArray} || Δ isa AbstractVectorOfArray
-                x = Δ isa AbstractVectorOfArray ? Δ.u[i] : Δ[i]
+            if Δ isa AbstractArray{<:AbstractArray}  Δ isa AbstractVectorOfArray || Δ isa Tangent
+                x = (Δ isa AbstractVectorOfArray || Δ isa Tangent) ? unthunk(Δ.u[i]) : Δ[i]
                 if _save_idxs isa Number
                     _out[_save_idxs] = x[_save_idxs]
                 elseif _save_idxs isa Colon
@@ -1691,7 +1691,6 @@ function DiffEqBase._concrete_solve_adjoint(prob::SciMLBase.AbstractODEProblem, 
                     vec(@view(_out[_save_idxs])) .= vec(x[_save_idxs])
                 end
             else
-                Δ = Δ isa Tangent ? unthunk(Δ.u) : Δ
                 if _save_idxs isa Number
                     _out[_save_idxs] = adapt(ArrayInterface.parameterless_type(u0),
                         reshape(Δ, prod(size(Δ)[1:(end - 1)]),
